33fe2669fb3a71d38434c065020c513cc5bc6984
Florian Lecocq <florian.lecocq@outlook.com>
2020-04-07 15:55:26 +0200

    Use roxygen with markdown

---
+++ SMM/R/SMM-package.R
@@ -0,0 +1,23 @@
+#' SMM : Semi-Markov and Markov Models
+#'
+#' @description This package performs parametric and non-parametric estimation 
+#' and simulation for multi-state discrete-time semi-Markov processes. For the 
+#' parametric estimation, several discrete distributions are considered for the
+#' sojourn times: Uniform, Geometric, Poisson, Discrete Weibull and Negative 
+#' Binomial. The non-parametric estimation concerns the sojourn time 
+#' distributions, where no assumptions are done on the shape of distributions. 
+#' Moreover, the estimation can be done on the basis of one or several sample 
+#' paths, with or without censoring at the beginning or/and at the end of the 
+#' sample paths. Estimation and simulation of discrete-time k-th order Markov 
+#' chains are also considered.
+#'
+#' To learn more about `SMM`, start with the vignettes:
+#' `browseVignettes(package = "SMM")`
+#'
+#' @keywords markov semi-markov simulation estimation censored
+#'
+#' @import utils
+#' @import stats
+#' @import seqinr
+#' @import DiscreteWeibull
+"_PACKAGE"
